c2be8e4cd61c75eda5942b6c0b9599bc570396b4
Atif <amnoori@users.noreply.github.com>
2017-04-20 13:14:30 -0700

    Added countdown Form

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,10 +5,11 @@ var Main = (props) => {
   return (
     <div>
       <Nav/>
-        <div>
-          <p>Main.jsx Rendered</p>
+      <div className="row">
+        <div className="column small-centered medium-6 large-4">
           {props.children}
         </div>
+      </div>
     </div>
   );
 }
